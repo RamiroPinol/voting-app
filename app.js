1306c280b4aec2360c911ae6b0145eaa68a9f336
RamiroPinol <ramirop@protonmail.com>
2017-01-24 18:03:46 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var reload = require('C:/Users/Ramiro/AppData/Roaming/npm/node_modules/reload');
+var app = express();
+var dataFile = require('./data/data.json');
+
+app.set('port', process.env.PORT || 3000 );
+
+app.get('/', function (req, res) {
+  res.send(JSON.stringify(dataFile.users[1].user_id))
+})
+
+var server = app.listen(app.get('port'), () => {
+  console.log('Listening on port ' + app.get('port'));
+});
+
+reload(server, app);
