cbb661034d970d0aec1f779e63dca4cd26e1b624
RamiroPinol <ramirop@protonmail.com>
2017-02-15 23:13:08 -0300

    updated packages required

--- app/app.js
+++ app/app.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var reload = require('reload');
+const express = require('express');
+const app = express();
+const reload = require('reload');
+const session = require('express-session');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var app = express();
-var dataFile = require('./data/data.json');
-var poll = require('./getPoll');
+const dataFile = require('./data/data.json');
+const poll = require('./getPoll');
+const login = require('./routes/login');
+const auth = require('./lib/auth');
+
+const configDB = require('./lib/database');
+mongoose.connect(configDB.url);
+
+require('./lib/passport')(passport);
 
 app.set('port', process.env.PORT || 3000 );
 app.set('appData', dataFile);
@@ -11,16 +24,22 @@ app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 app.set('Poll', poll)
 
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/newPoll'));
-app.use(require('./routes/user'));
+//app.use(require('./routes/login'));
 
+// required for passport
+app.use(session({ secret: 'stop!youshouldnotreadthisbecauseisasecret' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 var server = app.listen(app.get('port'), () => {
   console.log('Listening on port ' + app.get('port'));
 });
 
 reload(server, app);
-
-//<script src="/reload/reload.js"></script>
