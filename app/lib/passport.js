5fcdce163b0e843d509d49afc87255e3df96b179
RamiroPinol <ramirop@protonmail.com>
2017-02-16 23:22:15 -0300

    added unlink account routes and logic

--- app/lib/passport.js
+++ app/lib/passport.js
@@ -112,7 +112,22 @@ module.exports = (passport) => {
 
           // if the user is found, then log them in
           if (user) {
+            // if there is a user id already but no token (user was linked at one point and then removed)
+            // just add our token and profile information
+            if (!user.facebook.token) {
+              user.facebook.token = token;
+              user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+              user.facebook.email = profile.emails[0].value;
+
+              user.save(function(err) {
+                if (err) throw err;
+
+                return done(null, user);
+              });
+            }
+
             return done(null, user); // user found, return that user
+
           } else {
             // if there is no user found with that facebook id, create them
             var newUser = new User();
@@ -176,6 +191,20 @@ module.exports = (passport) => {
             return done(err);
 
           if (user) {
+
+            if (!user.twitter.token) {
+
+              user.twitter.token = token;
+              user.twitter.displayName  = profile.displayName;
+              user.twitter.username = profile.username;
+
+              user.save(function(err) {
+                if (err) throw err;
+
+                return done(null, user);
+              });
+            }
+
             return done(null, user);
 
           } else {
@@ -236,6 +265,20 @@ module.exports = (passport) => {
             return done(err);
 
           if (user) {
+
+            if (!user.google.token) {
+
+              user.google.token = token;
+              user.google.name  = profile.displayName;
+              user.google.email = profile.emails[0].value;
+
+              newUser.save(function(err) {
+                if (err) throw err;
+
+                return done(null, newUser);
+              });
+            }
+
             return done(null, user);
 
           } else {
@@ -256,7 +299,7 @@ module.exports = (passport) => {
         });
 
       } else {
-        
+
         var user = req.user;
 
         user.google.id    = profile.id;
